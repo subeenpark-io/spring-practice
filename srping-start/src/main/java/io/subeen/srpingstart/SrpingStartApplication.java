83f39f7b7a011f3f679d3080e6ad8da1177e7199
Subeen Park <psb021414@gmail.com>
2022-03-20 02:34:10 +0900

    init project

---
+++ srping-start/src/main/java/io/subeen/srpingstart/SrpingStartApplication.java
@@ -0,0 +1,13 @@
+package io.subeen.srpingstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrpingStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrpingStartApplication.class, args);
+	}
+
+}
